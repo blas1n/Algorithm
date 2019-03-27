cba816927da0f72001488ab1a1c47c300a5c1473
blAs1N <qazasa123@gmail.com>
2019-03-27 22:47:17 +0900

    Solve 2442.

---
+++ 2442.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int n, i, j;
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		for (j = 0; j < n - i; j++)
+			putchar(' ');
+		for (j = 0; j < i * 2 - 1; j++)
+			putchar('*');
+
+		putchar('\n');
+	}
+
+	return 0;
+}
