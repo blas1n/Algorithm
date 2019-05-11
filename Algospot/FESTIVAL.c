2c8144726cd5693edcf0582daefc517c6a0500df
blAs1N <qazasa123@gmail.com>
2019-05-11 20:51:15 +0900

    Solve FESTIVAL.

--- Algospot/FESTIVAL.c
+++ Algospot/FESTIVAL.c
@@ -3,24 +3,23 @@
 #define MIN(lhs, rhs) (((lhs) < (rhs)) ? (lhs) : (rhs))
 
 int board[1001];
-double cache[1001];
 
-double getMinAverage(int n, int l) {
+double GetMinAverage(int n, int l) {
 	double minAverage = 100.0;
+	double answer = 100.0;
 
 	for (int i = 0; i <= n - l; i++) {
 		double sum = 0.0;
-		for (int j = i; j < i + l - 1; j++) {
-			sum += board[j];
-		}
 
-		cache[i] = 100.0;
+		for (int j = i; j < i + l - 1; j++)
+			sum += board[j];
+
 		for (int j = i + l - 1; j < n; j++) {
 			sum += board[j];
 			double partialAverage = sum / (j - i + 1);
-			cache[i] = MIN(cache[i], partialAverage);
+			answer = MIN(answer, partialAverage);
 		}
-		minAverage = MIN(minAverage, cache[i]);
+		minAverage = MIN(minAverage, answer);
 	}
 
 	return minAverage;
@@ -38,7 +37,7 @@ int main() {
 			scanf("%d", &board[j]);
 		}
 
-		printf("%.12f\n", getMinAverage(n, l));
+		printf("%.12f\n", GetMinAverage(n, l));
 	}
 
 	return 0;
