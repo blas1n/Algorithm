c83f60bd1caedce313d6c38206c2e25235ca080f
blAs1N <qazasa123@gmail.com>
2019-06-03 14:30:34 +0900

    Solve 타일 장식물.

---
+++ Programmers/타일 장식물.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long cache[81] = { 0, 4, 6, };
+
+long long fib(int n) {
+	if (cache[n])
+		return cache[n];
+
+	cache[n] = fib(n - 1) + fib(n - 2);
+	return cache[n];
+}
+
+long long solution(int N) {
+	vector<int> a;
+	initializer_list
+	initializer_list<int>(a);
+	return solution(N);
+}
+
+int main() {
+	cout << solution(5) << endl;
+	return 0;
+}
