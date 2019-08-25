8999ecea2ff6591dad9bc44ad4167e7eb69f9ef2
blAs1N <hwiasin210@naver.com>
2019-08-25 14:56:49 +0900

    Solve 카펫

---
+++ Programmers/카펫.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+
+using namespace std;
+
+inline int GetRed(int x, int y)
+{
+	return (x - 2) * (y - 2);
+}
+
+inline int GetBrown(int x, int y)
+{
+	return (x - 1) * 2 + (y - 1) * 2;
+}
+
+vector<int> solution(int brown, int red) {
+	for (int x = 3; ; x++)
+		for (int y = 3; y <= x; y++)
+			if (GetRed(x, y) == red && GetBrown(x, y) == brown)
+				return vector<int>{x, y};
+}
