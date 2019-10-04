49de415bffa3873665a08bf4bb2bbb55478070af
blAs1N <hwiasin210@naver.com>
2019-10-04 18:31:59 +0900

    저장 오류 수정

--- Programmers/타일 장식물.cpp
+++ Programmers/타일 장식물.cpp
@@ -1,23 +1,9 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 long long cache[81] = { 0, 4, 6, };
 
-long long fib(int n) {
+long long solution(int n) {
 	if (cache[n])
 		return cache[n];
 
-	cache[n] = fib(n - 1) + fib(n - 2);
+	cache[n] = solution(n - 1) + solution(n - 2);
 	return cache[n];
-}
-
-long long solution(int N) {
-	vector<int> a;
-	initializer_list
-	initializer_list<int>(a);
-	return solution(N);
 }
