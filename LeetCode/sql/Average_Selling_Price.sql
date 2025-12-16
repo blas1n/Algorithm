bf7cf7bbf3c0b70664d3b9a24aaff69c985bd1ea
DongHwi Kim <qazasa123@gmail.com>
2025-12-16 11:48:22 +0900

    Solve Average Selling Price

---
+++ LeetCode/sql/Average_Selling_Price.sql
@@ -0,0 +1,3 @@
+SELECT p.product_id, COALESCE(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) as average_price FROM Prices p
+    LEFT JOIN UnitsSold u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+    GROUP BY p.product_id
