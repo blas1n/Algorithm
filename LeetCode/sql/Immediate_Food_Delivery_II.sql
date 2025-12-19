1f440bc44c80fc81dfdbe3031d53101294cdb4ce
DongHwi Kim <qazasa123@gmail.com>
2025-12-19 14:18:25 +0900

    Solve Immediate Food Delivery II

---
+++ LeetCode/sql/Immediate_Food_Delivery_II.sql
@@ -0,0 +1,5 @@
+SELECT ROUND(AVG(order_date = customer_pref_delivery_date) * 100.0, 2) as immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date) FROM Delivery GROUP BY customer_id
+);
