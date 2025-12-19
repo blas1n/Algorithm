1b0b40d5a90a968963706ed5068813acedcea112
DongHwi Kim <qazasa123@gmail.com>
2025-12-19 15:08:18 +0900

    Solve Game Play Analysis IV

---
+++ LeetCode/sql/Game_Play_Analysis_IV.sql
@@ -0,0 +1,5 @@
+SELECT ROUND(COUNT(player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+    SELECT player_id, MIN(event_date) FROM Activity GROUP BY player_id
+);
