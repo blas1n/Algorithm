f5d5124b23001b4ac47c47240c922ada69eb0208
DongHwi Kim <qazasa123@gmail.com>
2025-12-24 11:50:14 +0900

    Solve Primary Department for Each Employee

---
+++ LeetCode/sql/Primary_Department_for_Each_Employee.sql
@@ -0,0 +1,5 @@
+SELECT employee_id, department_id  FROM Employee
+GROUP BY employee_id HAVING COUNT(department_id) = 1
+UNION
+SELECT employee_id, department_id  FROM Employee
+WHERE primary_flag = 'Y'
