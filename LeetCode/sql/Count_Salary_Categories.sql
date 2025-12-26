2d1938f37e2d17c1c303db4ba8892cb55006de6f
DongHwi Kim <qazasa123@gmail.com>
2025-12-26 16:05:42 +0900

    Solve Count Salary Categories

---
+++ LeetCode/sql/Count_Salary_Categories.sql
@@ -0,0 +1,8 @@
+SELECT 'Low Salary' as category, COUNT(account_id) as accounts_count FROM Accounts
+WHERE income < 20000
+UNION
+SELECT 'Average Salary' as category, COUNT(account_id) as accounts_count FROM Accounts
+WHERE income >= 20000 and income <= 50000
+UNION
+SELECT 'High Salary' as category, COUNT(account_id) as accounts_count FROM Accounts
+WHERE income > 50000
