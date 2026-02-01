f0b13767fcb219d7a72d631af43f24de3f88c853
blAs1N <qazasa123@gmail.com>
2026-02-01 22:23:48 +0900

    Solve Target Index Search

---
+++ HackerRank/algorithm/Target_Index_Search.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'binarySearch' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY nums
+#  2. INTEGER target
+#
+
+def binarySearch(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    return -1
+
+if __name__ == '__main__':
+    nums_count = int(input().strip())
+
+    nums = []
+
+    for _ in range(nums_count):
+        nums_item = int(input().strip())
+        nums.append(nums_item)
+
+    target = int(input().strip())
+
+    result = binarySearch(nums, target)
+
+    print(result)
