0cd77770b19b1ef8ca17616d46a565640879d31c
DongHwi Kim <qazasa123@gmail.com>
2026-02-03 12:01:48 +0900

    Solve Count Number Pairs

---
+++ HackerRank/algorithm/Count_Number_Pairs.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'countAffordablePairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER budget
+#
+
+def countAffordablePairs(prices, budget):
+    answer = 0
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            if prices[i] + prices[j] > budget:
+                break
+            answer += 1
+    return answer
+
+if __name__ == '__main__':
+    prices_count = int(input().strip())
+
+    prices = []
+
+    for _ in range(prices_count):
+        prices_item = int(input().strip())
+        prices.append(prices_item)
+
+    budget = int(input().strip())
+
+    result = countAffordablePairs(prices, budget)
+
+    print(result)
