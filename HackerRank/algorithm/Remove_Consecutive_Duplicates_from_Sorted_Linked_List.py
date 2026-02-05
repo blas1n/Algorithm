0d9e7e40dccd7bef7ecdc5bfd20bedae75128083
DongHwi Kim <qazasa123@gmail.com>
2026-02-05 15:21:11 +0900

    Solve Remove Consecutive Duplicates from Sorted Linked List

---
+++ HackerRank/algorithm/Remove_Consecutive_Duplicates_from_Sorted_Linked_List.py
@@ -0,0 +1,80 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+
+
+#
+# Complete the 'deleteDuplicates' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def deleteDuplicates(head):
+    node, prev = head, None
+    while node:
+        if not prev or node.data != prev.data:
+            if prev:
+                prev.next = node
+            prev = node
+        node = node.next
+    if prev:
+        prev.next = None
+    return head
+
+if __name__ == '__main__':
+    head_count = int(input().strip())
+
+    head = SinglyLinkedList()
+
+    for _ in range(head_count):
+        head_item = int(input().strip())
+        head.insert_node(head_item)
+
+    result = deleteDuplicates(head.head)
+
+    print_singly_linked_list(result, '\n')
+    print()
