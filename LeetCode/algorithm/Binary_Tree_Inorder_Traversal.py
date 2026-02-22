645ee7a05e7c90912040376c47f86517a75aaa02
blAs1N <qazasa123@gmail.com>
2026-02-22 17:41:18 +0900

    Solve Binary Tree Inorder Traveral

---
+++ LeetCode/algorithm/Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        answer, stack, cur = [], [], root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            answer.append(cur.val)
+            cur = cur.right
+        return answer
